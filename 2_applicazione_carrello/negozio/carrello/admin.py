024d9d3346e766135d3928b78bc5175e262bce7a
Lorenzo Bugli <lorenzo@fiscozen.it>
2023-08-06 23:59:49 +0200

    feat(carrello): aggiunto admin

--- 2_applicazione_carrello/negozio/carrello/admin.py
+++ 2_applicazione_carrello/negozio/carrello/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Cart, Product, CartItem
+
+
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "price")
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+    inlines = [CartItemInline]
--- 2_applicazione_carrello/negozio/carrello/models.py
+++ 2_applicazione_carrello/negozio/carrello/models.py
@@ -7,13 +7,31 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    class Meta:
+        verbose_name = "Prodotto"
+        verbose_name_plural = "Prodotti"
+
+    def __str__(self):
+        return self.name
+
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through="CartItem")
+
+    class Meta:
+        verbose_name = "Carrello"
+        verbose_name_plural = "Carrelli"
+
+    def __str__(self):
+        return f"Carrello #{self.id} {self.user.username}"
 
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = "Elemento carrello"
+        verbose_name_plural = "Elementi carrello"
