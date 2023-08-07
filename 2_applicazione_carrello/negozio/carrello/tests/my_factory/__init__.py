1991a871e8777b100551242f81325dab2f05bae9
Lorenzo Bugli <lorenzo@fiscozen.it>
2023-08-07 09:04:54 +0200

    refactor(carrello): estrazione di product_factory

---
+++ 2_applicazione_carrello/negozio/carrello/tests/my_factory/__init__.py
@@ -0,0 +1,5 @@
+from .product_factory import ProductFactory
+
+__all__ = [
+    "ProductFactory",
+]
--- 2_applicazione_carrello/negozio/carrello/tests/test_unit.py
+++ 2_applicazione_carrello/negozio/carrello/tests/test_unit.py
@@ -1,19 +1,7 @@
-import random
-
-import factory
 from django.test import TestCase
 
 from carrello.models import Categories, Product
-
-
-class ProductFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Product
-
-    name = factory.Sequence(lambda n: f"Prodotto {n}")
-    description = factory.Sequence(lambda n: f"Descrizione prodotto {n}")
-    category = random.choice([option.value for option in Categories])
-    price = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
+from my_factory import ProductFactory
 
 
 class TestProductManager(TestCase):
